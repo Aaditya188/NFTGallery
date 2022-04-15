32847e340294a52a92c4f69e8fc40bf51beeca86
Aaditya Prakash <ap6885@srmist.edu.in>
2022-04-15 23:58:38 +0530

    NFT Gallery

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+import { Cursor } from './cursor';
+import { Grid } from './grid';
+import { preloadImages } from './utils';
+import connectx from './connect';
+
+
+// custom cursor
+const cursor = new Cursor(document.querySelector('.cursor'));
+
+// Preload  images
+preloadImages('.grid__item-img').then(() => {
+    // Remove loader (loading class)
+    document.body.classList.remove('loading');
+    
+    // Initialize grid
+    const grid = new Grid(document.querySelector('.grid'));
+    
+    // change cursor text status when hovering a grid item
+    grid.on('mouseEnterItem', itemTitle => cursor.DOM.text.innerHTML = itemTitle);
+    grid.on('mouseLeaveItem', _ => cursor.DOM.text.innerHTML = '');
+});
+
+// mouse effects on all links and others
+[...document.querySelectorAll('a, button, .grid__item')].forEach(link => {
+    link.addEventListener('mouseenter', () => cursor.enter());
+    link.addEventListener('mouseleave', () => cursor.leave());
+});
+
+let web3 = new Web3(window.ethereum)
+var btn = document.getElementById("connect");
+var game1 = document.getElementById("game");
+
+if(window.ethereum !== undefined){
+    web3.eth.getAccounts((e,accounts) => {
+        if(accounts.length != 0){
+            btn.innerHTML = "Enter Museum"
+        }
+    })
+}
+
+btn.addEventListener("click", function() {
+    connectx().then(()=> {
+        location.href = '../select.html';
+    }).catch(e => console.log(e))
+    
+}, false);
+game1.addEventListener("click", function() {
+    
+        location.href = 'https://virtual-van-gogh-museum-demo.vercel.app/';
+   
+    
+}, false);
